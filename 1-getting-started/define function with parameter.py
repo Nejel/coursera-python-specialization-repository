c550a492591dca862d173c71adaa0c613e98c866
Nejel <nejel@mail.ru>
2018-09-23 00:07:51 +0300

    funny pictures from Udemy added

--- 1-getting-started/define function with parameter.py
+++ 1-getting-started/define function with parameter.py
@@ -70,3 +70,15 @@ def string_length(mystring):
          return "Sorry, floats don't have length"
      else:
          return len(mystring)
+
+
+
+#several calls of function:
+temperatures = [10, -20, 100]
+
+def cel_to_fahr(celsius):
+    fahrenheit = celsius * 9/5 + 32
+    return fahrenheit
+
+for temperature in temperatures:
+    print(cel_to_fahr(temperature))
